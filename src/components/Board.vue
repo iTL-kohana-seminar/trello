e6a67d5810290aabd0bb1560045ddac0df3b1b33
ren-09 <im.from.jp99@gmail.com>
2021-11-28 01:59:04 +0900

    logoを変更

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,32 +1,31 @@
 <template>
   <div>
     <header>
-      my Trello
+      <img class="logo-img" src="../assets/img/logo_transparent.png" alt />
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <div class="list-index">
-      <draggable :list="lists"
-                  @end="movingList"
-                  class="list-index">
-        <List v-for="(item, index) in lists"
-              :key="item.id"
-              :title="item.title"
-              :cards="item.cards"
-              :listIndex="index"
-              @change="movingCard"
-        />
-        <ListAdd />
-      </draggable>
+        <draggable :list="lists" @end="movingList" class="list-index">
+          <List
+            v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :cards="item.cards"
+            :listIndex="index"
+            @change="movingCard"
+          />
+          <ListAdd />
+        </draggable>
       </div>
     </main>
   </div>
 </template>
 <script>
-import draggable from 'vuedraggable'
-import ListAdd from './ListAdd.vue'
-import List from './List'
-import { mapState } from 'vuex'
+import draggable from "vuedraggable";
+import ListAdd from "./ListAdd.vue";
+import List from "./List";
+import { mapState } from "vuex";
 
 export default {
   components: {
@@ -35,20 +34,24 @@ export default {
     draggable
   },
   computed: {
-    ...mapState([
-      'lists'
-    ]),
+    ...mapState(["lists"]),
     totalCardCount() {
-      return this.$store.getters.totalCardCount
+      return this.$store.getters.totalCardCount;
     }
   },
   methods: {
     movingCard: function() {
-      this.$store.dispatch('updateList', { lists: this.lists })
+      this.$store.dispatch("updateList", { lists: this.lists });
     },
     movingList: function() {
-      this.$store.dispatch('updateList', { lists: this.lists })
+      this.$store.dispatch("updateList", { lists: this.lists });
     }
   }
-}
+};
 </script>
+
+<style scoped>
+.logo-img {
+  width: 30%;
+}
+</style>
